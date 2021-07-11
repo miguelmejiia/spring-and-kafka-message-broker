495bd14424f989b34d4c8ed615ea816a7085a772
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-11 13:04:15 -0500

    Spring base project

---
+++ src/main/java/com/migsoft/kafka/spring/KafkaSpringMsbrApplication.java
@@ -0,0 +1,13 @@
+package com.migsoft.kafka.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpringMsbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpringMsbrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/migsoft/kafka/spring/KafkaSpringMsbrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migsoft.kafka.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringMsbrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
