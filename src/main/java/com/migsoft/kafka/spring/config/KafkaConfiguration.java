2245a9d53c9303f08f8ccb76ab6c9ee8fdadbc2e
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-11 19:19:17 -0500

    Remove unnecessary props

--- src/main/java/com/migsoft/kafka/spring/config/KafkaConfiguration.java
+++ src/main/java/com/migsoft/kafka/spring/config/KafkaConfiguration.java
@@ -18,10 +18,6 @@ public class KafkaConfiguration {
     public Map<String, Object> producerProperties () {
         Map<String, Object> props=new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ProducerConfig.RETRIES_CONFIG, 0);
-        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
-        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
-        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return props;
