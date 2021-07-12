4bab889d898359dc41f862468b4cd1d06e853190
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-11 19:41:50 -0500

    Implementation of asynchronous Callback

--- src/main/java/com/migsoft/kafka/spring/KafkaSpringMsbrApplication.java
+++ src/main/java/com/migsoft/kafka/spring/KafkaSpringMsbrApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaProducerException;
+import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @SpringBootApplication
 public class KafkaSpringMsbrApplication implements CommandLineRunner {
@@ -28,6 +32,23 @@ public class KafkaSpringMsbrApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		kafkaTemplate.send("java-topic", "Sample message");
+		ListenableFuture<SendResult <String, String>> future =  kafkaTemplate.send("java-topic", "Sample message");
+		future.addCallback(new KafkaSendCallback<String, String>() {
+
+			@Override
+			public void onSuccess(SendResult<String, String> stringStringSendResult) {
+				log.info("Message sent ", stringStringSendResult.getRecordMetadata().offset());
+			}
+
+			@Override
+			public void onFailure(Throwable ex) {
+				log.error("Error sending message ", ex);
+			}
+
+			@Override
+			public void onFailure(KafkaProducerException e) {
+				log.error("Error sending message ", e);
+			}
+		});
 	}
 }
