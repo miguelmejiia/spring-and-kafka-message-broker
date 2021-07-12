d20485d6440cae5eebd9721445bbbc3835940a91
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-11 19:14:51 -0500

    Create KafkaListrener to receive the messages

--- src/main/java/com/migsoft/kafka/spring/KafkaSpringMsbrApplication.java
+++ src/main/java/com/migsoft/kafka/spring/KafkaSpringMsbrApplication.java
@@ -1,10 +1,20 @@
 package com.migsoft.kafka.spring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class KafkaSpringMsbrApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(KafkaSpringMsbrApplication.class);
+
+	@KafkaListener(topics="java-topic", groupId = "java-group")
+	public void listen(String message){
+		log.info("Message received {} ", message);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaSpringMsbrApplication.class, args);
