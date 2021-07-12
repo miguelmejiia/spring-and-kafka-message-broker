925d702494a2205bc0bae92930b3bf034e20bff1
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-11 19:35:54 -0500

    Create producer

--- src/main/java/com/migsoft/kafka/spring/KafkaSpringMsbrApplication.java
+++ src/main/java/com/migsoft/kafka/spring/KafkaSpringMsbrApplication.java
@@ -2,14 +2,20 @@ package com.migsoft.kafka.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class KafkaSpringMsbrApplication {
+public class KafkaSpringMsbrApplication implements CommandLineRunner {
 
 	private static final Logger log = LoggerFactory.getLogger(KafkaSpringMsbrApplication.class);
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
 
 	@KafkaListener(topics="java-topic", groupId = "java-group")
 	public void listen(String message){
@@ -20,4 +26,8 @@ public class KafkaSpringMsbrApplication {
 		SpringApplication.run(KafkaSpringMsbrApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		kafkaTemplate.send("java-topic", "Sample message");
+	}
 }
