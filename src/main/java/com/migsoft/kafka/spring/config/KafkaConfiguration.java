b191e7aa5bba67441bd4911633c4042e31dea900
Miguel Mejia <miguelmejia92@outlook.com>
2021-07-11 19:16:50 -0500

    Add producer properties

--- src/main/java/com/migsoft/kafka/spring/config/KafkaConfiguration.java
+++ src/main/java/com/migsoft/kafka/spring/config/KafkaConfiguration.java
@@ -1,7 +1,9 @@
 package com.migsoft.kafka.spring.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -13,6 +15,18 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfiguration {
+    public Map<String, Object> producerProperties () {
+        Map<String, Object> props=new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return props;
+    }
+
     public Map<String, Object> consumerProperties () {
         Map<String, Object>props=new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
